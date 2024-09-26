386339c40da5e0a226d1654a90d4e5281d050631
Amairani Cancino Bello <83901707+acancinob@users.noreply.github.com>
2024-09-25 20:45:01 -0600

    Update Figuras_4y5.R

--- Grupo2/Figuras_4y5.R
+++ Grupo2/Figuras_4y5.R
@@ -1,24 +1,25 @@
-# Analysis of bacterial genomes evolved in the absence of phage
-# Reena Debray
-# Feb 1, 2022
-# Modified: Amairani Cancino Bello (04/Sep/2024) 
-# This script runs the ReproHack_data_figures4_5.RData variable to create the graphs.
+# Titulo: Análisis de genomas bacterianos evolucionados en ausencia de fagos
+# Creador: Reena Debray
+# Fecha: 1 de febrero de 2022
+# Modificado: Amairani Cancino Bello (04/Sep/2024)
+# Descripción: Este script ejecuta el archivo ReproHack_data_figures4_5.RData que contiene variables necesarias para crear las gráficas.
 
 
-#--- Packages ----
+#--- Paquetes ----
 library(ggplot2)
 library(viridis)
-# Load objects from the .RData file
-# The variable contains : 
-# Saving objects needed for the figures from 2_Genome_data_analysis.R script.
-# save(p_day6, p_day36, annotation_day6, test_gene_df_day6, annotation_day6, breseq_gene_props_day6, test_gene_df_day36, breseq_gene_props_day36, 
-#    file = "ReproHack_data_figures4_5.RData")
+# Cargar objetos desde el archivo .RData.
+# Guardar los objetos necesarios para las figuras del script 2_Genome_data_analysis.R
+# ReproHack_data_figures4_5.RData contiene las variables:
+# p_day6, p_day36, annotation_day6, test_gene_df_day6, annotation_day6, breseq_gene_props_day6, test_gene_df_day36 y breseq_gene_props_day36.
+
 getwd()
+#--- Cargar las variables----
 load("ReproHack_data_figures4_5.RData")
 
-# Figure 4A y 4B
-#--- Fig. 4A ---- 
-# Creation and display of the graph
+#--- Figuras 4A y 4B---- 
+# Fig. 4A 
+# Creación y visualización de la gráfica
 p_day6 <- ggplot(test_gene_df_day6, aes(label, sim_coef)) +
   geom_boxplot(outlier.shape = NA, width = 0.4, size = 0.8) +
   geom_jitter(width = 0.05, size = 2, alpha = 0.2) +
@@ -32,24 +33,25 @@ p_day6 <- ggplot(test_gene_df_day6, aes(label, sim_coef)) +
 
 print(p_day6)
 
-#--- Plot Fig 4B---- 
+# Fig. 4B 
 ggplot(breseq_gene_props_day6, aes(top_gene, res_gene, fill = percent_pops * 100)) +
   geom_tile(color = "grey80") +
   theme_classic(base_size = 14) +
   theme(
-    axis.text.x = element_text(angle = 90, hjust = 1, size = 10, family = "Arial")  # Define all adjustments in one call
+    axis.text.x = element_text(angle = 90, hjust = 1, size = 10, family = "Arial")  # Definir todos los ajustes en una sola llamada
   ) +
   scale_fill_viridis(
     name = "Populations\nwith mutation (%)",
-    limits = c(0, 100),  # Define scale limits
-    breaks = c(0, 25, 50, 75, 100)  # Define specific breaks to display
+    limits = c(0, 100),  # Definir los límites de la escala
+    breaks = c(0, 25, 50, 75, 100)  # Definir cortes específicos a mostrar
   ) +
   ylab("") +
   xlab("") +
   ggtitle("Day 6 of experimental evolution")
 
 
-#--- Fig. 5A----
+#---Figuras 5A y 5B ----
+# Fig. 5A
 p_day36 <- ggplot(test_gene_df_day36, aes(label, sim_coef)) +
   geom_boxplot(outlier.shape = NA, width = 0.4, size = 0.8) +
   geom_jitter(width = 0.05, size = 2, alpha = 0.2) +
@@ -62,7 +64,7 @@ p_day36 <- ggplot(test_gene_df_day36, aes(label, sim_coef)) +
 
 print(p_day36)
 
-#--- Fig. 5B----
+# Fig. 5B 
 ggplot(breseq_gene_props_day36, aes(top_gene, res_gene, fill = percent_pops * 100)) +
   geom_tile(color = "grey80") +
   theme_classic(base_size = 14) +
